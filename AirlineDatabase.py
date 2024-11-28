3d7d7cf342a58709e7d82f804e1ebcc0ec9653e1
AyakSalu <aewyheq@gmail.com>
2024-11-28 16:18:43 +0300

    Satın al geliştirildi

--- AirlineDatabase.py
+++ AirlineDatabase.py
@@ -19,9 +19,14 @@ cursor =conn.cursor()
 if __name__ == '__main__':
     
 
-    command = """SELECT max(Booking_ID)  
-            from bookings 
-            """ 
+    command = """
+        INSERT INTO bookings(Flight_ID, Passenger_ID, Booking_Date, Seat_Column, Seat_Row, Booking_Status, Seat_Type)
+        VALUES (1,200, '2069-11-28 12:00:00', 'A' ,11 ,'Confirmed','Economy');
+        """ 
+    command = """
+        Select Passenger_id from Passengers 
+        where Passport_Number = 'P8460896'
+        """
     values = executeCommand(command)
-    print(values)
+    print(len(values))
 
--- DatabaseUI.py
+++ DatabaseUI.py
@@ -5,7 +5,7 @@ from datetime import datetime
 maxBookingIdCommand = """SELECT max(Booking_ID)  
             from bookings 
             """ 
-next_booking_id = executeCommand(maxBookingIdCommand) + 1
+next_booking_id = executeCommand(maxBookingIdCommand)[0][0]+1
 def filtrele():
     text = personIdTextBox.get("1.0",END)
     global lst
@@ -28,13 +28,52 @@ def biletSatinAl(biletNumarasi):
     seat_col="C"
     seat_row=0
     seat_type = "Economy"
-    print("deneme")
     def insertToTable():
-        command = """
-        INSERT INTO bookings (%s, %s, %s, %s,%s,%s,%s,%s)
-        """,next_booking_id,biletNumarasi,datetime.now(),seat_col,seat_row,"Occupied",seat_type
-        next_booking_id += 1
+        passportNumber = personPassportText.get("1.0",END)
+        fname = personNameText.get("1.0",END)
+        lname = personLastNameText.get("1.0",END)
+        phoneNumber = personPhoneNumberText.get("1.0",END)
+        email = personEmailText.get("1.0",END)
+        amount = personPurchaseAmount.get("1.0",END)
+        paymentMethod = personPurchaseType.get("1.0",END)
+        time = datetime.now()
 
+
+
+        #ilk önce passanger tablosuna sonra  booking tablosuna sonrada payment tablosuna ekleme yapılmalı
+        #aynı pasaport numarasına sahip passanger varsa eklenmiyecek
+        command = """
+        Select Passanger_id from Passangers 
+        where Passport_number = %s
+        """,passportNumber
+        result = executeCommand(command)
+        if len(result) != 0 :
+            command = """
+                INSERT INTO Passengers (Fname, Lname, Passport_Number, Phone_Number, Email) 
+                VALUES ('%s', '%s', '%s', '%s', '%s')
+            """,fname,lname,passportNumber,phoneNumber,email
+        
+        command = """
+        Select Passenger_id from Passengers 
+        where Passport_Number = '%s'
+        """,passportNumber
+        personid = executeCommand(command)
+
+        command = """
+        INSERT INTO Bookings(Flight_ID, Passenger_ID, Booking_Date, Seat_Column, Seat_Row, Booking_Status, Seat_Type) 
+            VALUES (%s, %s, '%s', '%s', %s, '%s', '%s');
+        """,biletNumarasi,personid,time,seat_col,seat_row,"Confirmed",seat_type
+        
+        command = """
+        Select Booking_id from Bookings 
+        where Flight_ID = '%s'
+        """,biletNumarasi
+        booking_id = executeCommand(command)
+
+        command = """
+        INSERT INTO Payments (Booking_ID, Amount, Payment_Date, Payment_Method) VALUES 
+        (%s, %s, '%s', '%s');
+        """,booking_id,amount,time,paymentMethod
         executeCommand(command)
         
     newWindow = Toplevel(root)
@@ -56,6 +95,31 @@ def biletSatinAl(biletNumarasi):
 
     personSeatColumnTextBox = Text(newWindow,height=1,width=20)
     personSeatColumnTextBox.grid(row=2,column=2) 
+
+    personEmailLabel = Label(newWindow, text = "Email")
+    personEmailLabel.grid(sticky='W',row=5,column=1)
+    personEmailText = Text(newWindow,height=1,width=20)
+    personEmailText.grid(sticky='W',row=5,column=2)
+
+    personNameLabel = Label(newWindow, text = "İsim")
+    personNameLabel.grid(sticky='W',row=6,column=1)
+    personNameText = Text(newWindow,height=1,width=20)
+    personNameText.grid(sticky='W',row=6,column=2)
+
+    personLastNameLabel = Label(newWindow, text = "Soyisim")
+    personLastNameLabel.grid(sticky='W',row=7,column=1)
+    personLastNameText = Text(newWindow,height=1,width=20)
+    personLastNameText.grid(sticky='W',row=7,column=2)
+
+    personPassportTypeLabel = Label(newWindow, text = "Passaport ya da TCKN")
+    personPassportTypeLabel.grid(sticky='W',row=8,column=1)
+    personPassportText = Text(newWindow,height=1,width=20)
+    personPassportText.grid(sticky='W',row=8,column=2)
+
+    personPhoneNumberLabel = Label(newWindow, text = "Telefon Numarası")
+    personPhoneNumberLabel.grid(sticky='W',row=9,column=1)
+    personPhoneNumberText = Text(newWindow,height=1,width=20)
+    personPhoneNumberText.grid(sticky='W',row=9,column=2)
 
     buyButton = Button(newWindow,text='Satın Al',height=4,command=insertToTable,relief='solid') 
     buyButton.grid(row=1,column=3,rowspan=4)
@@ -115,19 +179,22 @@ class TableInitial:
         resetTable(self)
         self.e = Label(root, text="Flight_Id",width=18, fg='black',borderwidth=1,relief='solid',
                                font=('Arial',8))
-        
+        self.e.configure(background='white')
         self.e.grid(row=2, column=3)
         self.Labels.append(self.e)  
         self.e = Label(root,text="Arrival Airport", width=18, fg='black',borderwidth=1,relief='solid',
                                font=('Arial',8))
+        self.e.configure(background='white')
         self.e.grid(row=2, column=4)
         self.Labels.append(self.e)  
         self.e = Label(root,text="Departure Airport", width=18, fg='black',borderwidth=1,relief='solid',
                                font=('Arial',8))
+        self.e.configure(background='white')
         self.e.grid(row=2, column=5)
         self.Labels.append(self.e)  
         self.e = Label(root,text="Airline", width=18, fg='black',borderwidth=1,relief='solid',
                                font=('Arial',8))
+        self.e.configure(background='white')
         self.e.grid(row=2, column=6)
         self.Labels.append(self.e)  
         # code for creating table
@@ -140,12 +207,14 @@ class TableInitial:
                     text = "Not Available"
                 self.e = Label(root,text=text ,width=18, fg='black',borderwidth=1,relief='solid',
                                font=('Arial',8))
-                self.Labels.append(self.e)            
+                self.Labels.append(self.e)    
+                self.e.configure(background='white')        
                 self.e.grid(row=i+3, column=j+3)
             self.e = Button(root,text='Satın Al',command=lambda : biletSatinAl(lst[i][0]),padx=0, pady=0,width=40, height=11,compound="center",
                        image=pixel)
             self.Labels.append(self.e)
             self.e.grid(row=i+3, column=j+4)
+            self.e.configure(background='white')
 class TableSearched:
     Labels = list()
     def __init__(self,root,table):
@@ -157,17 +226,21 @@ class TableSearched:
                                font=('Arial',8))
         
         self.e.grid(sticky='W',row=2, column=3)
+        self.e.configure(background='white')
         self.e = Label(root,text="Arrival Airport", width=18, fg='black',borderwidth=1,relief='solid',
                                font=('Arial',8))
         self.e.grid(sticky='W',row=2, column=4)
+        self.e.configure(background='white')
         self.Labels.append(self.e)  
         self.e = Label(root,text="Departure Airport", width=18, fg='black',borderwidth=1,relief='solid',
                                font=('Arial',8))
         self.e.grid(sticky='W',row=2, column=5)
+        self.e.configure(background='white')
         self.Labels.append(self.e)  
         self.e = Label(root,text="Airline", width=18, fg='black',borderwidth=1,relief='solid',
                                font=('Arial',8))
         self.e.grid(sticky='W',row=2, column=6)
+        self.e.configure(background='white')
         self.Labels.append(self.e)  
         # code for creating table
         for i in range(total_rows):
@@ -181,6 +254,7 @@ class TableSearched:
                                font=('Arial',8))
                 self.Labels.append(self.e)            
                 self.e.grid(sticky='W',row=i+3, column=j+3)
+                self.e.configure(background='white')
   
         
 
@@ -201,20 +275,24 @@ total_rows = len(lst)
 total_columns = len(lst[0])
 
 root  = Tk()
-root .title("AirlineManagement")
-root .geometry('1600x700+100+100')
+root.title("AirlineManagement")
+root.geometry('1600x700+100+100')
+root.configure(background='white')
 pixel = tk.PhotoImage(width=1, height=1)
 #window = ScrolledFrame(root)
 #window.pack(side = 'left',expand=True, fill='both')
 
 
-personIdTextBoxLabel = Label(root, text = "Biletlerinizi görmek ya da bilet almak için id'nizi giriniz")
+personIdTextBoxLabel = Label(root, text = "Biletlerinizi görmek ya da bilet almak için Passaport Numaranızı giriniz")
+personIdTextBoxLabel.configure(background='white',foreground='black')
 personIdTextBoxLabel.grid(sticky='W',row=1,column=1,columnspan=5) 
-personIdTextBox = Text(root,height=1,width=20)
+personIdTextBox = Text(root,height=1,width=20,borderwidth=1)
+personIdTextBox.configure(background='white',foreground='black')
 personIdTextBox.grid(row=1,column=6,columnspan=4) 
 
 
 searchButton = Button(root,text='Search',command=filtrele)
+searchButton.configure(background='white',foreground='black')
 searchButton.grid(row=1,column=10) 
 table = TableInitial(root)
 
