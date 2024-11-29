83d2e8fe32d8be21c19403b89455ff99a0986ecd
Ahmet Salih <43313563+ahmetsalihkaya@users.noreply.github.com>
2024-11-29 13:40:56 +0300

    Add files via upload

--- AirlineDatabase.py
+++ AirlineDatabase.py
@@ -12,6 +12,7 @@ def executeCommand(command):
         mysql.connection.commit()
         return data
 
+
 def insert_passengers(Fname, Lname, Passport_Number, Phone_Number, Email):
     command = """
     Select Passenger_id from passengers 
@@ -40,6 +41,17 @@ def get_avaliable_flights(departure_country, arrival_country):
                % (departure_country, arrival_country))
     return executeCommand(command)
 
+
+def check_booking_availability(flight_id, seat_row, seat_col, passenger_id):
+    # seat biri tarafından alınmış mı? ya da vatandaş zaten bu uçuştan bilet almış mı?
+    command = (""" Select * from bookings
+    Where Flight_ID = '%s' AND ((Seat_Row = '%s' AND Seat_Column = '%s') OR Passenger_ID = '%s') """ % (flight_id, seat_row, seat_col, passenger_id))
+    booking_available = len(executeCommand(command)) == 0
+
+    #ikisi de boş kümeyse TRUE dönecek
+    return booking_available
+
+
 def remove_booked_flight(flight_id, passport_num):
     command = ("""DELETE FROM bookings 
     WHERE bookings.Flight_ID = '%s' 
@@ -54,6 +66,7 @@ def get_next_bookid():
                 """
     return executeCommand(maxBookingIdCommand)[0][0] + 1
 
+
 app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
@@ -65,17 +78,20 @@ mysql = MySQL(app)
 
 user_id = -1
 
+
 # Helper: Admin check
 def is_admin():
     return user_id == -1
 
+
 def get_flight_id(flight_code):
     command = (""" SELECT flights.Flight_ID from flights where flights.Flight_Code = '%s' """ % (flight_code))
-    return executeCommand(command)[0][0] #id'yi int olarak döner
+    return executeCommand(command)[0][0]  # id'yi int olarak döner
+
 
 if __name__ == '__main__':
     with app.app_context():
-        #cursor = mysql.connection.cursor()
+        # cursor = mysql.connection.cursor()
         command = """
             INSERT INTO bookings(Flight_ID, Passenger_ID, Booking_Date, Seat_Column, Seat_Row, Booking_Status, Seat_Type)
             VALUES (1,100, '2069-11-28 12:00:00', 'A' ,11 ,'Confirmed','Economy');
@@ -86,13 +102,21 @@ if __name__ == '__main__':
             where Passport_Number = 'P8460896'
         """
 
+        # values = executeCommand(command)
+        # values = get_avaliable_flights("India", "Australia")
+        # print(values)  # For INSERT, this will likely print 0 as no rows are returned
 
+        # command = ("""SELECT * from bookings WHERE bookings.Flight_ID = '%s' AND
+        # AND bookings.Passenger_ID IN  """ % (30, 123))
+        # command = """ Select Distinct(Passenger_ID) from passengers WHERE passengers.Passport_Number = '%s' """ % (123)
 
-        #values = executeCommand(command)
-        #values = get_avaliable_flights("India", "Australia")
-        #print(values)  # For INSERT, this will likely print 0 as no rows are returned
+    #     command = ("""Delete FROM bookings
+    # WHERE bookings.Flight_ID = '%s'
+    # AND bookings.Passenger_ID IN (Select Distinct(Passenger_ID) from passengers WHERE passengers.Passport_Number = '%s')"""
+    #                % (30, 123))
+        # command = """ (SELECT unique(Passenger_ID) from passengers WHERE Passport_Number = '%s') """ % (123)
+        # print(executeCommand(command))
 
-        command = (""" SELECT flights.Flight_ID from flights where flights.Flight_Code = '%s' """ % ("FC6566"))
-        print(executeCommand(command))
-        print(get_flight_id("FC6566"))
-        #mysql.connection.commit()  # Ensure changes are saved to the database for INSERT/UPDATE/DELETE
+        print(check_booking_availability(30, 1000, 'Z', 104))
+        # print(remove_booked_flight(30, 123))
+        # mysql.connection.commit()  # Ensure changes are saved to the database for INSERT/UPDATE/DELETE
